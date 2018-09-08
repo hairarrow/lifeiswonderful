66566acc08ea2be715437ff7840f6b9f3bad1bf4
Emmanuel Herrero <emmanuelherrero@gmail.com>
2018-09-07 20:07:19 -0500

    cube

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const Cube = () => (
+  <div className="cube">
+    <div className="face front" />
+    <div className="face side" />
+    <div className="face top" />
+  </div>
+);
+
+const CubesRow = () => (
+  <div className="cubes__row">
+    <Cube />
+    <Cube />
+    <Cube />
+    <Cube />
+  </div>
+);
+
+const CubesColumn = () => (
+  <div className="cubes__column">
+    <CubesRow />
+    <CubesRow />
+    <CubesRow />
+    <CubesRow />
+  </div>
+);
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className="App-title">Hello World!</h1>
+        <div className="cubes">
+          <CubesColumn />
+          <CubesColumn />
+          <CubesColumn />
+          <CubesColumn />
+        </div>
       </div>
     );
   }
