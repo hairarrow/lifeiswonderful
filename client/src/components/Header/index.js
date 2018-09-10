cc0793d7b77ec5dc752a611e4a24ba99fd08861b
Emmanuel Herrero <emmanuelherrero@gmail.com>
2018-09-09 23:15:48 -0500

    better animations on card hover

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,11 @@
 import "./header.css";
-import LOGO from "./logo.svg";
+import Logo from "../Logo";
 import React from "react";
 
 const Header = () => (
   <nav className="header">
     <div className="header__brand">
-      <img className="header__brand-logo" src={LOGO} alt="Metacard" />
+      <Logo />
       <h1 className="header__brand-title">Metacard</h1>
     </div>
   </nav>
